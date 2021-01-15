a421a4c3db12f94ff704ac95829e4885143a8042
Alex Marko <msfs@touching.cloud>
2021-01-15 14:52:19 +0300

    missing files

---
+++ MSFS Kinetic Assistant/NearbyInfoResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MSFS_Kinetic_Assistant
+{
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    public struct NearbyInfoResponse
+    {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        public String Title;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        public String Category;
+        public double Latitude;
+        public double Longitude;
+        public double Altitude;
+        public double Airspeed;
+    }
+}
